0fda85bc038645923fed61f54ea5bba5fe9e3890
coworksnabe <atsuki.watanabe@co-works.co.jp>
2023-03-27 14:43:23 +0900

    #01 モックの作成 要振替授業管理（管理者）

---
+++ source/web-testea/resources/views/pages/admin/modal/transfer_required-modal.blade.php
@@ -0,0 +1,38 @@
+@extends('pages.common.modal')
+
+@section('modal-body')
+
+{{-- テーブル --}}
+<x-bs.table :hover=false :vHeader=true>
+
+    <tr>
+        <th>校舎</th>
+        <td>久我山</td>
+    </tr>
+    <tr>
+        <th>授業日時</th>
+        <td>2023/01/30 4限 15:00</td>
+    </tr>
+    <tr>
+        <th>生徒名</th>
+        <td>CWテスト生徒１</td>
+    </tr>
+    <tr>
+        <th>講師名</th>
+        <td>CWテスト教師１０１</td>
+    </tr>
+    <tr>
+        <th>教科</th>
+        <td>数学</td>
+    </tr>
+
+</x-bs.table>
+
+@overwrite
+
+@section('modal-button')
+
+{{-- 打ち合わせのみ更新ボタンを表示 --}}
+<x-button.edit href="{{ route('transfer_check-new') }}" icon="" caption="振替情報登録" />
+
+@overwrite
---
+++ source/web-testea/resources/views/pages/admin/transfer_required.blade.php
@@ -0,0 +1,81 @@
+@extends('adminlte::page')
+
+@section('title', '要振替授業一覧')
+
+@section('content')
+
+{{-- 検索フォーム --}}
+<x-bs.card :search=true>
+
+    <x-bs.row>
+        <x-bs.col2>
+            <x-input.select id="kinds" caption="校舎" :select2=true>
+                <option value="1">久我山</option>
+                <option value="2">西永福</option>
+                <option value="3">本郷</option>
+            </x-input.select>
+        </x-bs.col2>
+    </x-bs.row>
+
+    <x-bs.row>
+        <x-bs.col2>
+            <x-input.text caption="生徒名" id="student_name" :rules=$rules />
+        </x-bs.col2>
+        <x-bs.col2>
+            <x-input.text caption="講師名" id="teacher_name" :rules=$rules />
+        </x-bs.col2>
+    </x-bs.row>
+
+    <x-bs.row>
+        <x-bs.col2>
+            <x-input.date-picker caption="授業日 From" id="date_from" />
+        </x-bs.col2>
+        <x-bs.col2>
+            <x-input.date-picker caption="授業日 To" id="date_to" />
+        </x-bs.col2>
+    </x-bs.row>
+</x-bs.card>
+
+{{-- 結果リスト --}}
+<x-bs.card-list>
+
+    {{-- テーブル --}}
+    <x-bs.table :button=true>
+
+        {{-- テーブルタイトル行 --}}
+        <x-slot name="thead">
+            <th>校舎</th>
+            <th>授業日</th>
+            <th>生徒名</th>
+            <th>講師名</th>
+            <th></th>
+        </x-slot>
+
+        {{-- テーブル行 --}}
+        <tr>
+            <td>久我山</td>
+            <td>2023/01/30</td>
+            <td>CWテスト生徒１</td>
+            <td>CWテスト教師１０１</td>
+            <td>
+                <x-button.list-dtl />
+            </td>
+        </tr>
+        <tr>
+            <td>久我山</td>
+            <td>2023/01/31</td>
+            <td>CWテスト生徒２</td>
+            <td>CWテスト教師１０２</td>
+            <td>
+                <x-button.list-dtl />
+            </td>
+        </tr>
+
+    </x-bs.table>
+
+</x-bs.card-list>
+
+{{-- 詳細 --}}
+@include('pages.admin.modal.transfer_required-modal')
+
+@stop
