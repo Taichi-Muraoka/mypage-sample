53e1b53664f01fa8e46487041ffc5c73bba47515
coworksnabe <atsuki.watanabe@co-works.co.jp>
2023-05-24 16:38:15 +0900

    #1 モックの修正 会員登録・編集（管理者） ・入力項目の修正

--- source/web-testea/resources/views/pages/admin/member_mng-input.blade.php
+++ source/web-testea/resources/views/pages/admin/member_mng-input.blade.php
@@ -31,8 +31,8 @@
     {{-- 共通フォーム --}}
     <x-input.text caption="生徒名" id="name" :rules=$rules :editData=$editData/>
     <x-input.text caption="生徒名カナ" id="name_kana" :rules=$rules :editData=$editData/>
-    {{-- <x-input.select caption="学年" id="cls_cd" :select2=true :blank=false :editData=$editData :mastrData=$classes/> --}}
-    <x-input.select id="cls_cd" caption="学年" :select2=true >
+    {{-- <x-input.select caption="学年" id="grade_cd" :select2=true :blank=false :editData=$editData :mastrData=$classes/> --}}
+    <x-input.select id="grade_cd" caption="学年" :select2=true >
         <option value="1">高3</option>
         <option value="2">高2</option>
         <option value="3">高1</option>
@@ -40,31 +40,29 @@
         <option value="5">中2</option>
         <option value="6">中1</option>
     </x-input.select>
-    <x-input.text caption="電話番号（生徒）" id="tel_student" :rules=$rules :editData=$editData/>
-    <x-input.text caption="電話番号（保護者）" id="tel_guardian" :rules=$rules :editData=$editData/>
-    <x-input.text caption="Email（生徒）" id="email_student" :rules=$rules :editData=$editData/>
-    <x-input.text caption="Email（保護者）" id="email_guardian" :rules=$rules :editData=$editData/>
-    <x-input.select caption="受講コース" id="courses" :select2=true :blank=false :editData=$editData>
-        <option value="1">個別指導コース</option>
-        <option value="2">集団授業</option>
-    </x-input.select>
-    <x-input.select caption="所属校舎" id="roomcd" :select2=true :blank=false :editData=$editData>
-        <option value="1">久我山</option>
-        <option value="2">西永福</option>
-        <option value="3">下高井戸</option>
-        <option value="4">駒込</option>
-        <option value="5">日吉</option>
-        <option value="6">自由が丘</option>
-    </x-input.select>
-    <x-input.text caption="所属学校" id="school" :rules=$rules :editData=$editData/>
-    <x-input.select caption="受験生フラグ" id="jukensei" :select2=true :blank=false :editData=$editData>
+    <x-input.text caption="所属学校（小）" id="school_cd_e" :rules=$rules :editData=$editData/>
+    <x-input.text caption="所属学校（中）" id="school_cd_j" :rules=$rules :editData=$editData/>
+    <x-input.text caption="所属学校（高）" id="school_cd_h" :rules=$rules :editData=$editData/>
+    <x-input.select caption="受験生フラグ" id="is_jukensei" :select2=true :blank=false :editData=$editData>
         <option value="1">非受験生</option>
         <option value="2">受験生</option>
     </x-input.select>
-    <x-input.select caption="表示フラグ" id="display_flag" :select2=true :blank=false :editData=$editData>
-        <option value="1">表示</option>
-        <option value="2">非表示</option>
+    <x-input.text caption="生徒電話番号" id="tel_stu" :rules=$rules :editData=$editData/>
+    <x-input.text caption="保護者電話番号" id="tel_par" :rules=$rules :editData=$editData/>
+    <x-input.text caption="生徒メールアドレス" id="email_stu" :rules=$rules :editData=$editData/>
+    <x-input.text caption="保護者メールアドレス" id="email_par" :rules=$rules :editData=$editData/>
+    <x-input.select caption="ログインID種別" id="login_kind" :select2=true :blank=false :editData=$editData>
+        <option value="1">生徒</option>
+        <option value="2">保護者</option>
     </x-input.select>
+    <x-input.select caption="会員ステータス" id="stu_status" :select2=true :blank=false :editData=$editData>
+        <option value="0">見込み客</option>
+        <option value="1">入会</option>
+        <option value="2">退会処理中</option>
+        <option value="3">退会</option>
+    </x-input.select>
+    <x-input.date-picker caption="入会日" id="enter_date" />
+    <x-input.date-picker caption="退会日" id="leave_date" />
 
     {{-- hidden --}}
     <x-input.hidden id="sid" :editData=$editData />
