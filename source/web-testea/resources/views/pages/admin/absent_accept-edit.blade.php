1cbe75f99e7805814186e0c20e669b9c133a8db7
coworksnabe <atsuki.watanabe@co-works.co.jp>
2023-06-19 11:39:06 +0900

    #1 モック修正 欠席申請受付　項目変更（管理者）

--- source/web-testea/resources/views/pages/admin/absent_accept-edit.blade.php
+++ source/web-testea/resources/views/pages/admin/absent_accept-edit.blade.php
@@ -55,8 +55,7 @@
         :mastrData=$statusList /> --}}
     <x-input.select caption="ステータス" id="state" :select2=true :editData=$editData>
         <option value="1">未対応</option>
-        <option value="2">受付</option>
-        <option value="3">対応済</option>
+        <option value="2">対応済</option>
     </x-input.select>
 
     {{-- hidden --}}
