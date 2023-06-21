b6d3547c557ce005b678cc2780f478f30b0e12a1
coworksnabe <atsuki.watanabe@co-works.co.jp>
2023-06-21 15:40:28 +0900

    #1 モック修正 生徒カルテ・バッジ管理　項目変更（管理者）

--- source/web-testea/resources/views/pages/admin/member_mng-detail.blade.php
+++ source/web-testea/resources/views/pages/admin/member_mng-detail.blade.php
@@ -94,7 +94,7 @@
 
         {{-- テーブルタイトル行 --}}
         <x-slot name="thead">
-            <th width="15%">付与日</th>
+            <th width="15%">認定日</th>
             <th width="10%">バッジ種別</th>
             <th width="10%">校舎</th>
             <th width="15%">担当者名</th>
