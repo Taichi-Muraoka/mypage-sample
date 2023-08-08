d632b5cd90cd6f7087ae803b56311cccd9c2f59d
coworksnabe <atsuki.watanabe@co-works.co.jp>
2023-08-08 14:10:23 +0900

    #1 モック修正 講師授業集計（管理者） ・検索項目に校舎追加 ・授業数をコース別に時間単位で表示 ・CSVダウンロードボタン ・詳細モーダルの項目追加

--- source/web-testea/resources/views/pages/admin/modal/tutor_class-modal.blade.php
+++ source/web-testea/resources/views/pages/admin/modal/tutor_class-modal.blade.php
@@ -6,15 +6,21 @@
 <x-bs.table :hover=false :vHeader=true>
     <tr>
         <th width="70%">体験授業生徒</th>
+        <th>体験授業日</th>
         <th>会員ステータス</th>
+        <th>入会日</th>
     </tr>
     <tr>
         <td>CWテスト生徒１０</td>
+        <td>2023/06/17</td>
         <td>見込客</td>
+        <td></td>
     </tr>
     <tr>
         <td>CWテスト生徒１１</td>
+        <td>2023/06/10</td>
         <td>入会</td>
+        <td>2023/06/25</td>
     </tr>
 </x-bs.table>
 
--- source/web-testea/resources/views/pages/admin/tutor_class.blade.php
+++ source/web-testea/resources/views/pages/admin/tutor_class.blade.php
@@ -9,6 +9,18 @@
 
     <x-bs.row>
         <x-bs.col2>
+            <x-input.select id="roomcd" caption="校舎" :select2=true >
+                <option value="1">久我山</option>
+                <option value="2">西永福</option>
+                <option value="3">下高井戸</option>
+                <option value="4">駒込</option>
+                <option value="5">日吉</option>
+                <option value="6">自由が丘</option>
+            </x-input.select>
+        </x-bs.col2>
+    </x-bs.row>
+    <x-bs.row>
+        <x-bs.col2>
             <x-input.date-picker caption="授業日 From" id="holiday_date_from"/>
         </x-bs.col2>
         <x-bs.col2>
@@ -21,19 +33,30 @@
 {{-- 結果リスト --}}
 <x-bs.card-list :mock=true>
 
+    {{-- カードヘッダ右 --}}
+    <x-slot name="tools">
+        <x-button.submit-exec caption="CSVダウンロード" icon="fas fa-download" />
+    </x-slot>
+
     {{-- テーブル --}}
     <x-bs.table :button=true>
 
         {{-- テーブルタイトル行 --}}
         <x-slot name="thead">
             <th width="10%">講師ID</th>
-            <th width="20%">講師名</th>
-            <th width="10%">実施授業時間（分）</th>
-            <th width="10%">代講授業数（受）</th>
-            <th width="10%">緊急代講授業数（受）</th>
-            <th width="10%">代講授業数（出）</th>
-            <th width="10%">緊急代講授業数（出）</th>
-            <th width="10%">初回授業数</th>
+            <th width="15%">講師名</th>
+            <th width="5%">個別</th>
+            <th width="6%">１対２</th>
+            <th width="6%">１対３</th>
+            <th width="5%">集団</th>
+            <th width="5%">家庭教師</th>
+            <th width="5%">演習</th>
+            <th width="6%">ハイプラン</th>
+            <th width="6%">代講授業数（受）</th>
+            <th width="6%">緊急代講授業数（受）</th>
+            <th width="6%">代講授業数（出）</th>
+            <th width="6%">緊急代講授業数（出）</th>
+            <th width="6%">初回授業数</th>
             <th></th>
         </x-slot>
 
@@ -41,7 +64,13 @@
         <tr>
             <td>100101</td>
             <td>CWテスト教師１０１</td>
-            <td class="t-price">1,980</td>
+            <td class="t-price">18</td>
+            <td class="t-price">3</td>
+            <td class="t-price">4.5</td>
+            <td class="t-price">3</td>
+            <td class="t-price">6</td>
+            <td class="t-price">2</td>
+            <td class="t-price">10</td>
             <td class="t-price">0</td>
             <td class="t-price">1</td>
             <td class="t-price">1</td>
@@ -54,7 +83,13 @@
         <tr>
             <td>100102</td>
             <td>CWテスト教師１０２</td>
-            <td class="t-price">1,080</td>
+            <td class="t-price">18</td>
+            <td class="t-price">3</td>
+            <td class="t-price">4.5</td>
+            <td class="t-price">3</td>
+            <td class="t-price">6</td>
+            <td class="t-price">2</td>
+            <td class="t-price">10</td>
             <td class="t-price">0</td>
             <td class="t-price">0</td>
             <td class="t-price">0</td>
