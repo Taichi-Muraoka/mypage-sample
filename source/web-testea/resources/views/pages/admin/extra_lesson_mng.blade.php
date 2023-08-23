1b65ceadeb9d5c86453f9d658709ea0288b97e79
coworksnabe <atsuki.watanabe@co-works.co.jp>
2023-08-23 11:01:37 +0900

    #1 モック修正 追加授業依頼受付（管理者） ・生徒名リストボックス変更 ・授業追加ボタン追加 ・指導ブース削除 ・コース名追加

--- source/web-testea/resources/views/pages/admin/extra_lesson_mng.blade.php
+++ source/web-testea/resources/views/pages/admin/extra_lesson_mng.blade.php
@@ -36,7 +36,11 @@
 
     <x-bs.row>
         <x-bs.col2>
-            <x-input.text caption="生徒名" id="name" :rules=$rules />
+            <x-input.select caption="生徒名" id="student" :select2=true :editData="$editData">
+                <option value="1">CWテスト生徒１</option>
+                <option value="2">CWテスト生徒２</option>
+                <option value="3">CWテスト生徒３</option>
+            </x-input.select>
         </x-bs.col2>
     </x-bs.row>
 
@@ -45,20 +49,15 @@
 {{-- 結果リスト --}}
 <x-bs.card-list>
 
-    {{-- カードヘッダ右 --}}
-    <x-slot name="tools">
-        <x-button.new href="{{ route('extra_lesson_mng-new') }}" :small=true caption='追加授業登録'/>
-    </x-slot>
-
     {{-- テーブル --}}
     <x-bs.table :button=true>
 
         {{-- テーブルタイトル行 --}}
         <x-slot name="thead">
-            <th width="15%">依頼日</th>
+            <th>依頼日</th>
             <th>校舎</th>
-            <th width="20%">生徒名</th>
-            <th width="15%">ステータス</th>
+            <th>生徒名</th>
+            <th>ステータス</th>
             <th></th>
         </x-slot>
 
@@ -70,6 +69,7 @@
             <td>未対応</td>
             <td>
                 <x-button.list-dtl />
+                <x-button.list-edit href="{{ route('extra_lesson_mng-new') }}" caption="授業追加"/>
                 <x-button.list-edit href="{{ route('extra_lesson_mng-edit',1) }}" />
             </td>
         </tr>
