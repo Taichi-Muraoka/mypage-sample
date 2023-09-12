e57f95be18edb4154ff743e97a80dbb1807c0ad4
coworksnabe <atsuki.watanabe@co-works.co.jp>
2023-09-12 15:21:25 +0900

    #11 modelファイル作成 60～72

---
+++ source/web-testea/app/Models/new/Salary.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * 給与情報 - モデル
+ */
+class Salary extends Model
+{
+
+    // モデルの共通処理
+    use \App\Traits\ModelTrait;
+
+    // 論理削除
+    use SoftDeletes;
+
+    /**
+     * モデルと関連しているテーブル
+     *
+     * @var string
+     */
+    protected $table = 'salaries';
+
+    /**
+     * テーブルの主キー
+     *
+     * @var array
+     */
+
+    protected $primaryKey = 'salary_id';
+
+    /**
+     * IDが自動増分されるか
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * 複数代入する属性
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'tutor_id',
+        'salary_date',
+        'total_amount',
+        'memo',
+    ];
+
+    /**
+     * 日付項目の定義
+     *
+     * @var array
+     */
+    protected $dates = [];
+
+    /**
+     * 属性に対するモデルのデフォルト値
+     *
+     * @var array
+     */
+    protected $attributes = [];
+
+    /**
+     * 配列に含めない属性
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at', 'updated_at', 'deleted_at'
+    ];
+
+    /**
+     * モデルの「初期起動」メソッド
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        // 更新時、空白をnullに変換する処理
+        self::whenSaveEmptyToNull();
+
+        // テーブル操作時、ログを残す処理
+        self::saveToLog();
+    }
+
+    //-------------------------------
+    // 項目定義
+    //-------------------------------
+
+    /**
+     * テーブル項目の定義
+     *
+     * @return array
+     */
+    protected static function getFieldRules()
+    {
+        static $_fieldRules = [
+            'salary_id' => ['integer'],
+            'tutor_id' => ['integer'],
+            'salary_date' => ['date_format:Y-m-d'],
+            'total_amount' => ['integer', 'min:0', 'max:99999999'],
+            'memo' => ['string', 'max:1000'],
+        ];
+        return $_fieldRules;
+    }
+
+    //-------------------------------
+    // 検索条件
+    //-------------------------------
+
+}
