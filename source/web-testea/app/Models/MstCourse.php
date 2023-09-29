a22b0899129878db092c8fcccb7e1bd1a9bd79da
coworksnabe <atsuki.watanabe@co-works.co.jp>
2023-09-29 15:13:57 +0900

    #19 実装 - マスタ管理 コースマスタ string入力での数字のみ許可（運用管理）

--- source/web-testea/app/Models/MstCourse.php
+++ source/web-testea/app/Models/MstCourse.php
@@ -104,7 +104,7 @@ class MstCourse extends Model
     protected static function getFieldRules()
     {
         static $_fieldRules = [
-            'course_cd' => ['integer', 'min:10000', 'max:99999', 'digits:5'],
+            'course_cd' => ['string', 'digits:5'],
             'name' => ['string', 'max:50'],
             'course_kind' => ['integer', 'in:1,2,3'],
             'summary_kind' => ['integer', 'in:0,1,2,3,4,5,6,7,8,9,10']
