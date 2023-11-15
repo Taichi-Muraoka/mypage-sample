4d03a99cfa01b50479397c0ce48d7b70d4d5fd4f
taichi.muraoka <taichi.okamu@gmail.com>
2023-11-15 18:49:00 +0900

    #73 実装 - お知らせ（生徒・講師）

--- source/web-testea/resources/views/pages/mypage-common/modal/notice-modal.blade.php
+++ source/web-testea/resources/views/pages/mypage-common/modal/notice-modal.blade.php
@@ -3,80 +3,6 @@
 @section('modal-body')
 
 {{-- テーブル --}}
-{{-- モック用 --}}
-{{-- その他　講師用 --}}
-{{-- <x-bs.table :hover=false :vHeader=true :smartPhoneModal=true vShow="item.id == 6">
-    <tr>
-        <th width="35%">通知日</th>
-        <td>2023/01/16</td>
-    </tr>
-    <tr>
-        <th>タイトル</th>
-        <td>生徒欠席連絡</td>
-    </tr>
-    <tr>
-        <th>送信元校舎</th>
-        <td>本部</td>
-    </tr>
-    <tr>
-        <th>送信者名</th>
-        <td>本部管理者</td>
-    </tr>
-    <tr>
-        <th>内容</th>
-        <td class="nl2br">CWテスト生徒１さんより、授業欠席の連絡がありました。<br>授業日時：2023/01/23 16:00<br>校舎：久我山</td>
-    </tr>
-</x-bs.table> --}}
-
-{{-- 給与 --}}
-{{-- <x-bs.table :hover=false :vHeader=true :smartPhoneModal=true vShow="item.id == 7">
-    <tr>
-        <th width="35%">通知日</th>
-        <td>2023/01/16</td>
-    </tr>
-    <tr>
-        <th>タイトル</th>
-        <td>給与のお知らせ</td>
-    </tr>
-    <tr>
-        <th>送信元校舎</th>
-        <td>本部</td>
-    </tr>
-    <tr>
-        <th>送信者名</th>
-        <td>本部管理者</td>
-    </tr>
-    <tr>
-        <th>内容</th>
-        <td class="nl2br">給与のお知らせです。</td>
-    </tr>
-</x-bs.table> --}}
-
-{{-- 追加請求 --}}
-{{-- <x-bs.table :hover=false :vHeader=true :smartPhoneModal=true vShow="item.id == 8">
-    <tr>
-        <th width="35%">通知日</th>
-        <td>2023/01/16</td>
-    </tr>
-    <tr>
-        <th>タイトル</th>
-        <td>追加請求申請に関するお知らせ</td>
-    </tr>
-    <tr>
-        <th>送信元校舎</th>
-        <td>本部</td>
-    </tr>
-    <tr>
-        <th>送信者名</th>
-        <td>本部管理者</td>
-    </tr>
-    <tr>
-        <th>内容</th>
-        <td class="nl2br">追加請求申請に関するお知らせです。</td>
-    </tr>
-</x-bs.table> --}}
-
-{{-- 本番用 --}}
 <x-bs.table :hover=false :vHeader=true :smartPhoneModal=true>
 
     <tr>
@@ -110,15 +36,6 @@
 {{-- モーダルの追加のボタン --}}
 @section('modal-button')
 
-{{-- モック用 --}}
-{{-- 講師のみ --}}
-{{-- @can('tutor') --}}
-{{-- 特別期間講習連絡へのリンク --}}
-{{-- <x-button.edit vShow="item.id== 2" vueHref="'{{ route('season_tutor') }}'" icon="" caption="特別期間講習連絡 " /> --}}
-{{-- <x-button.edit vShow="item.id== 7" vueHref="'{{ route('salary') }}'" icon="" caption="給与明細 " /> --}}
-{{-- <x-button.edit vShow="item.id== 8" vueHref="'{{ route('surcharge') }}'" icon="" caption="追加請求申請 " /> --}}
-{{-- @endcan --}}
-
 {{-- 本番用 --}}
 {{-- 生徒のみ --}}
 @can('student')
@@ -140,6 +57,12 @@
 {{-- 特別期間講習連絡へのリンク --}}
 <x-button.edit vShow="item.type == {{ App\Consts\AppConst::CODE_MASTER_14_6 }}"
     vueHref="'{{ route('season_tutor') }}'" icon="" caption="特別期間講習連絡 " />
+{{-- 給与情報へのリンク --}}
+<x-button.edit vShow="item.type == {{ App\Consts\AppConst::CODE_MASTER_14_9 }}"
+    vueHref="'{{ route('salary') }}'" icon="" caption="給与情報 " />
+{{-- 追加請求へのリンク --}}
+<x-button.edit vShow="item.type == {{ App\Consts\AppConst::CODE_MASTER_14_10 }}"
+    vueHref="'{{ route('surcharge') }}'" icon="" caption="追加請求 " />
 @endcan
 
 @overwrite
