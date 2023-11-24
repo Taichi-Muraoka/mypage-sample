6f7dc5542b3c84866b9312e0cc3be41e23e31571
coworksnabe <atsuki.watanabe@co-works.co.jp>
2023-11-24 10:38:58 +0900

    #87 修正 - 講師管理機能（運用管理）学校検索モーダル共通化 生徒管理・受験校管理の修正も含む

--- source/web-testea/app/Http/Controllers/Admin/TutorMngController.php
+++ source/web-testea/app/Http/Controllers/Admin/TutorMngController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\Traits\FuncCalendarTrait;
 use App\Http\Controllers\Traits\FuncSalaryTrait;
 use App\Http\Controllers\Traits\FuncTutorDetailTrait;
+use App\Http\Controllers\Traits\FuncSchoolSearchTrait;
 use App\Consts\AppConst;
 use App\Models\MstTutorGrade;
 use App\Models\MstSchool;
@@ -43,6 +44,9 @@ class TutorMngController extends Controller
 
     // 機能共通処理：給与
     use FuncSalaryTrait;
+
+    // 機能共通処理：学校検索モーダル
+    use FuncSchoolSearchTrait;
 
     /**
      * コンストラクタ
@@ -878,7 +882,7 @@ class TutorMngController extends Controller
             // forceDelete・insertとする
             // 既存データ削除
             TutorSubject::where('tutor_id', $request['tutor_id'])
-                    ->forceDelete();
+                ->forceDelete();
 
             // 教科選択がある場合のみ保存処理を行う
             if (!empty($request['subject_groups'])) {
@@ -1440,50 +1444,8 @@ class TutorMngController extends Controller
      */
     public function searchSchool(Request $request)
     {
-        // バリデーション。NGの場合はレスポンスコード422を返却
-        Validator::make($request->all(), $this->rulesForSearchSchool())->validate();
-
-        // formを取得
-        $form = $request->all();
-
-        // クエリ作成
-        $query = MstSchool::query();
-
-        // 学校種の絞り込み条件
-        $query->SearchSchoolKind($form);
-
-        // 設置区分の絞り込み条件
-        $query->SearchEstablishKind($form);
-
-        // 学校コードの絞り込み条件
-        $query->SearchSchoolCd($form);
-
-        // 学校名の絞り込み条件
-        $query->SearchSchoolName($form);
-
-        // バッジ一覧取得
-        $schoolList = $query
-            ->select(
-                'mst_schools.school_cd',
-                'mst_schools.school_kind_cd',
-                // コードマスタの名称(学校種コード)
-                'mst_codes_49.name as school_kind_name',
-                'mst_schools.establish_kind',
-                // コードマスタの名称(設置区分)
-                'mst_codes_50.name as establish_name',
-                'mst_schools.name as school_name',
-            )
-            // コードマスターとJOIN（学校種コード）
-            ->sdLeftJoin(CodeMaster::class, function ($join) {
-                $join->on('mst_schools.school_kind_cd', '=', 'mst_codes_49.code')
-                    ->where('mst_codes_49.data_type', AppConst::CODE_MASTER_49);
-            }, 'mst_codes_49')
-            // コードマスターとJOIN（設置区分）
-            ->sdLeftJoin(CodeMaster::class, function ($join) {
-                $join->on('mst_schools.establish_kind', '=', 'mst_codes_50.code')
-                    ->where('mst_codes_50.data_type', AppConst::CODE_MASTER_50);
-            }, 'mst_codes_50')
-            ->orderby('mst_schools.school_cd');
+        // 検索結果を取得
+        $schoolList = $this->getSchoolList($request);
 
         // ページネータで返却
         return $this->getListAndPaginator($request, $schoolList);
@@ -1500,47 +1462,5 @@ class TutorMngController extends Controller
         // リクエストデータチェック
         $validator = Validator::make($request->all(), $this->rulesForSearchSchool());
         return $validator->errors();
-    }
-
-    /**
-     * バリデーションルールを取得(学校検索用)
-     *
-     * @return array ルール
-     */
-    private function rulesForSearchSchool()
-    {
-        $rules = array();
-
-        // 独自バリデーション: リストのチェック 学校種
-        $validationSchoolKindList =  function ($attribute, $value, $fail) {
-
-            // リストを取得し存在チェック
-            $schoolKinds = $this->mdlMenuFromCodeMaster(AppConst::CODE_MASTER_49);
-            if (!isset($schoolKinds[$value])) {
-                // 不正な値エラー
-                return $fail(Lang::get('validation.invalid_input'));
-            }
-        };
-
-        // 独自バリデーション: リストのチェック 設置区分
-        $validationEstablishKindList =  function ($attribute, $value, $fail) {
-
-            // リストを取得し存在チェック
-            $establish = $this->mdlMenuFromCodeMaster(AppConst::CODE_MASTER_50);
-            if (!isset($establish[$value])) {
-                // 不正な値エラー
-                return $fail(Lang::get('validation.invalid_input'));
-            }
-        };
-
-        $rules += MstSchool::fieldRules('school_kind_cd', [$validationSchoolKindList]);
-        $rules += MstSchool::fieldRules('establish_kind', [$validationEstablishKindList]);
-        // 学校コードまたは学校名のどちらか必須
-        // 学校名はid名がテーブル項目名と異なるためルールを継承するかたちで記述した
-        $ruleName = MstSchool::getFieldRule('name');
-        $rules += ['school_name' =>  array_merge($ruleName, ['required_without_all:school_cd'])];
-        $rules += MstSchool::fieldRules('school_cd', ['required_without_all:school_name']);
-
-        return $rules;
     }
 }
