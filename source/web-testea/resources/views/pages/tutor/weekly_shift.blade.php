2324b995c11992b73c16efb0e8fcfcd81f85e424
Mikiko Toba <mikiko.toba@co-works.co.jp>
2023-12-01 15:37:52 +0900

    #119 実装 - 空き時間登録機能（講師） 実装完了

--- source/web-testea/resources/views/pages/tutor/weekly_shift.blade.php
+++ source/web-testea/resources/views/pages/tutor/weekly_shift.blade.php
@@ -7,7 +7,7 @@
 {{-- フォーム --}}
 <x-bs.card :form=true>
 
-  <p>現在入っているレギュラー授業以外で、授業実施可能なコマ（空き時間）を登録してください。</p>
+  <p>現在入っているレギュラー授業以外で、授業実施可能な時限（空き時間）を登録してください。</p>
 
   {{-- チェックボックスのエラー時のメッセージ --}}
   <x-bs.form-group name="chkWs" />
@@ -25,26 +25,27 @@
     </x-slot>
 
     {{-- 二重ループで組み立てる --}}
-    @for ($i = 0; $i < count($timeList); $i++) <tr>
-      <td class="tt">{{$timeList[$i]}}</td>
+    @foreach ($periodList as $periodKey => $periodVal)
+        {{-- 時限を表示 --}}
+        <td class="tt">{{$periodKey}}限</td>
 
       @foreach ($weekdayList as $weekdayKey => $weekdayVal)
       <td>
         {{-- チェックボックス。裏でクリックされた時間帯を保持している --}}
-        <x-input.checkbox id="{{$weekdayKey}}_{{$timeIdList[$i]}}" class="chk-wt" name="chkWs" :icheck=false
-          value="{{$weekdayKey}}_{{$timeIdList[$i]}}" :editData=$editData :exceptData=$exceptData />
+        <x-input.checkbox id="{{$weekdayKey}}_{{$periodKey}}" class="chk-wt" name="chkWs" :icheck=false
+          value="{{$weekdayKey}}_{{$periodKey}}" :editData=$editData :exceptData=$exceptData />
 
         {{-- 表のDiv --}}
-        <div class="chk-t" data-wt="{{$weekdayKey}}_{{$timeIdList[$i]}}" v-on:click="timeClick"></div>
+        <div class="chk-t" data-wt="{{$weekdayKey}}_{{$periodKey}}" v-on:click="timeClick"></div>
       </td>
       @endforeach
 
       </tr>
-    @endfor
+    @endforeach
 
   </x-bs.table>
 
-  <p><br>黒色：レギュラー授業　　緑色：空き時間</p>
+  <p><br>黒色：レギュラー授業&nbsp;／&nbsp;緑色：空き時間</p>
 
   {{-- フッター --}}
   <x-slot name="footer">
