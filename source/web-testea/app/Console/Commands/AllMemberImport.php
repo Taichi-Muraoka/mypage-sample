c575fd538a68e3d18425f1cbbf3e35919381c176
taichi.muraoka <taichi.okamu@gmail.com>
2024-01-27 17:43:23 +0900

    #186 実装 - 学年更新管理(運用管理)

--- source/web-testea/app/Console/Commands/AllMemberImport.php
+++ source/web-testea/app/Console/Commands/AllMemberImport.php
@@ -3,16 +3,13 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Log;
-use App\Exceptions\ReadDataValidateException;
-use App\Models\ExtStudentKihon;
-use App\Models\Account;
+use App\Models\MstSystem;
+use App\Models\MstGrade;
+use App\Models\Student;
 use App\Models\BatchMng;
-use App\Http\Controllers\Traits\CtrlCsvTrait;
-use App\Http\Controllers\Traits\CtrlFileTrait;
+use App\Http\Controllers\Traits\CtrlDateTrait;
 use Carbon\Carbon;
 use App\Consts\AppConst;
 
@@ -21,16 +18,16 @@ use App\Consts\AppConst;
  */
 class AllMemberImport extends Command
 {
-    // CSV共通処理
-    use CtrlCsvTrait;
-    use CtrlFileTrait;
+
+    // 年度取得用
+    use CtrlDateTrait;
 
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'command:allMemberImport {path} {account_id}';
+    protected $signature = 'command:allMemberImport';
 
     /**
      * The console command description.
@@ -58,89 +55,64 @@ class AllMemberImport extends Command
     public function handle()
     {
         try {
-
-            // CSVファイルのパスと実行者のアカウントIDを受け取る
-            $path = $this->argument("path");
-            $account_id = $this->argument("account_id");
-            $datas = [];
-
-            Log::info("Batch allMemberImport Start, PATH: {$path}, ACCOUNT_ID: {$account_id}");
+            Log::info("Batch allMemberImport Start.");
 
             $now = Carbon::now();
 
             // バッチ管理テーブルにレコード作成
             $batchMng = new BatchMng;
-            $batchMng->batch_type = AppConst::BATCH_TYPE_1;
+            $batchMng->batch_type = AppConst::BATCH_TYPE_11;
             $batchMng->start_time = $now;
             $batchMng->batch_state = AppConst::CODE_MASTER_22_99;
-            $batchMng->adm_id = $account_id;
+            $batchMng->adm_id = null;
             $batchMng->save();
 
+            // バッヂID
             $batch_id = $batchMng->batch_id;
 
-            try {
-                // Zipを解凍し、ファイルパス一覧を取得
-                $opPathList = $this->unzip($path);
-                // 今回は1件しか無いので、1件目を取得
-                if (count($opPathList) != 1) {
-                    throw new ReadDataValidateException(Lang::get('validation.invalid_file')
-                        . "(csvファイル数不正)");
-                }
+            // システムマスタから現年度を取得
+            $system = MstSystem::where('key_id', AppConst::SYSTEM_KEY_ID_1)
+                ->firstOrFail();
 
-                $csvPath = $opPathList[0];
+            // 生徒情報を取得
+            $students = Student::whereNotIn('grade_cd', [13, 14, 15, 16])
+                ->select(
+                    'student_id',
+                    'birth_date',
+                    'grade_cd',
+                    'grade_year'
+                )
+                ->get();
 
-                // CSVデータの読み込み
-                $datas = $this->readData($csvPath);
-                if (empty($datas)) {
-                    throw new ReadDataValidateException(Lang::get('validation.invalid_file')
-                        . "(データ件数不正)");
-                }
-
-                // Zip解凍ファイルのクリーンアップ
-                $this->unzipCleanUp($opPathList);
-            } catch (ReadDataValidateException  $e) {
-                // 通常は事前にバリデーションするのでここはありえないのでエラーとする
-                throw $e;
-            }
+            // 年度はじめを取得
+            $next_year_start_date = date('Y', strtotime('+10 month')) . '0401';
 
             // トランザクション(例外時は自動的にロールバック)
-            DB::transaction(function () use ($datas, $batch_id) {
+            DB::transaction(function () use ($system, $students, $next_year_start_date, $batch_id) {
 
-                $updateCount = 0;
-                // スケジュールテーブルを登録・更新
-                foreach ($datas as $data) {
+                // システムマスタの現年度更新
+                $system->value_num = $system->value_num + 1;
+                $system->save();
 
-                    // レコードが存在するかチェック(キーを指定)
-                    $exists = ExtStudentKihon::where('sid', "=", $data['sid'])->exists();
+                // 生徒の学年更新
+                foreach ($students as $student) {
+                    // 誕生日
+                    $birthday = $student->birth_date->format('Ymd');
 
-                    if ($exists) {
-                        $updateCount++;
-                        $extStudentKihon = ExtStudentKihon::where(['sid' => $data['sid']])
-                            ->firstOrFail();
+                    // 年齢
+                    $age = floor(($next_year_start_date - $birthday) / 10000);
 
-                        $extStudentKihon['name'] = $data['name'];
-                        $extStudentKihon['cls_cd'] = $data['cls_cd'];
-                        $extStudentKihon['mailaddress1'] = $data['mailaddress1'];
-                        $extStudentKihon['enter_date'] = $data['enter_date'];
-                        $extStudentKihon['disp_flg'] = $data['disp_flg'];
-                        $extStudentKihon['updtime'] = $data['updtime'];
-                        $extStudentKihon->save();
+                    // 新年度の年齢での学年を取得
+                    $grade_age = MstGrade::where('age', $age)
+                        ->select('grade_cd', 'age')
+                        ->firstOrFail();
 
-                        // 既存のアカウントがある場合はアカウントのメールアドレスも更新する。
-                        $accountExists = Account::where('account_id', '=', $data['sid'])
-                            ->where('account_type', '=', AppConst::CODE_MASTER_7_1)
-                            ->exists();
-
-                        if ($accountExists) {
-
-                            $account = Account::where('account_id', '=', $data['sid'])
-                                ->where('account_type', '=', AppConst::CODE_MASTER_7_1)
-                                ->firstOrFail();
-
-                            $account['email'] = $data['mailaddress1'];
-                            $account->save();
-                        }
-                    }
+                    // 学年コード更新
+                    $student->grade_cd = $grade_age->grade_cd;
+                    // 学年設定年度更新
+                    $student->grade_year = $system->value_num;
+                    // 更新
+                    $student->save();
                 }
 
                 // バッチ管理テーブルのレコードを更新：正常終了
@@ -152,8 +124,7 @@ class AllMemberImport extends Command
                         'updated_at' => $end
                     ]);
 
-                $updateCount = (string) $updateCount;
-                Log::info("Update {$updateCount} Records. allMemberImport Succeeded.");
+                Log::info("allMemberImport Succeeded.");
             });
         } catch (\Exception  $e) {
             // バッチ管理テーブルのレコードを更新：異常終了
@@ -167,90 +138,5 @@ class AllMemberImport extends Command
             // この時点では補足できないエラーとして、詳細は返さずエラーとする
             Log::error($e);
         }
-        // 念のため明示的に捨てる
-        $datas = null;
-    }
-
-    /**
-     * アップロードされたファイルを読み込む
-     * バリデーションも行う
-     * 
-     * @param $path
-     * @return array データ
-     */
-    private function readData($path)
-    {
-        // レスポンス
-        $datas = [];
-
-        // ヘッダ行
-        $csvHeaders = ["sid", "name", "cls_cd", "mailaddress1", "disp_flg", "updtime", "upduser", "enter_date"];
-
-        $headers = [];
-
-        // CSV読み込み
-        $file = $this->readCsv($path, "sjis");
-
-        // 1行ずつ取得
-        foreach ($file as $i => $line) {
-
-            // 1行目がヘッダ行
-            if ($i === 0) {
-                //-------------
-                // ヘッダ行
-                //-------------
-                $headers = $line;
-
-                // [バリデーション] ヘッダが想定通りかチェック
-                if ($headers !== $csvHeaders) {
-                    throw new ReadDataValidateException(Lang::get('validation.invalid_file')
-                        . "(" . config('appconf.upload_file_csv_name_A05') . "：ヘッダ行不正)");
-                }
-
-                continue;
-            }
-
-            // [バリデーション] データ行の列の数のチェック
-            if (count($line) !== count($csvHeaders)) {
-                throw new ReadDataValidateException(Lang::get('validation.invalid_file')
-                    . "(" . config('appconf.upload_file_csv_name_A05') . "：データ列数不正)");
-            }
-
-            // headerをもとに、値をセットしたオブジェクトを生成
-            $values = array_combine($headers, $line);
-
-            // [バリデーション] データ行の値のチェック
-            $rules = [];
-            $rules += ExtStudentKihon::fieldRules('sid', ['required']);
-            $rules += ExtStudentKihon::fieldRules('name', ['required']);
-            $rules += ExtStudentKihon::fieldRules('cls_cd', ['required']);
-            $rules += ExtStudentKihon::fieldRules('mailaddress1', ['required']);
-            $rules += ExtStudentKihon::fieldRules('enter_date', [], '_csv');
-            $rules += ExtStudentKihon::fieldRules('disp_flg', ['required']);
-            $rules += ExtStudentKihon::fieldRules('updtime', ['required'], '_csv');
-
-            $validator = Validator::make($values, $rules);
-            if ($validator->fails()) {
-                throw new ReadDataValidateException(Lang::get('validation.invalid_file')
-                . config('appconf.upload_file_csv_name_A05')
-                . "：データ項目不正( 生徒No=" . $values['sid'] . " )");
-            }
-
-            $values['sid'] = (int) $values['sid'];
-            $values['disp_flg'] = (int) $values['disp_flg'];
-            foreach ($values as $key => $val) {
-                // 空白はnullに変換
-                if ($values[$key] === '') {
-                    $values[$key] = null;
-                }
-            }
-
-            // 不要な項目を削除
-            unset($values['upduser']);
-
-            array_push($datas, $values);
-        }
-
-        return $datas;
     }
 }
--- source/web-testea/app/Http/Controllers/Admin/AllMemberImportController.php
+++ source/web-testea/app/Http/Controllers/Admin/AllMemberImportController.php
@@ -4,15 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Lang;
-use App\Exceptions\ReadDataValidateException;
-use App\Models\ExtStudentKihon;
 use App\Models\BatchMng;
-use App\Models\AdminUser;
 use App\Models\CodeMaster;
 use App\Consts\AppConst;
+use App\Libs\AuthEx;
 
 /**
  * 学年情報取込 - コントローラ
@@ -42,27 +37,29 @@ class AllMemberImportController extends Controller
     {
         // クエリ作成
         $query = BatchMng::query();
-        $students = $query
+
+        // バッヂリスト取得
+        $batchList = $query
             ->select(
+                'batch_id',
                 'start_time',
                 'end_time',
                 'batch_state',
-                'mst_codes.name AS batch_state_name', 
+                // コードマスタの名称（バッチ状態）
+                'mst_codes.name as state_name',
             )
+            // コードマスターとJOIN
             ->sdLeftJoin(CodeMaster::class, function ($join) {
                 $join->on('batch_state', '=', 'mst_codes.code')
-                    ->where('mst_codes.data_type', AppConst::CODE_MASTER_22);
+                    ->where('data_type', AppConst::CODE_MASTER_22);
             })
-            ->where('batch_type', '=', AppConst::BATCH_TYPE_1)
+            // バッチ種別で絞り込み
+            ->where('batch_type', AppConst::CODE_MASTER_23_11)
             ->orderBy('start_time', 'desc');
 
         // ページネータで返却
-        return $this->getListAndPaginator($request, $students);
+        return $this->getListAndPaginator($request, $batchList);
     }
-
-    //==========================
-    // 取込
-    //==========================
 
     /**
      * 初期画面
@@ -71,222 +68,14 @@ class AllMemberImportController extends Controller
      */
     public function index()
     {
+        // 教室管理者の場合は画面を表示しない
+        if (AuthEx::isRoomAdmin()) {
+            return $this->illegalResponseErr();
+        }
 
         return view('pages.admin.all_member_import', [
-            'rules' => $this->rulesForInput(),
             'editData' => ["this_year"=>"2022","next_year"=>"2023"]
         ]);
     }
 
-    /**
-     * 取込処理
-     *
-     * @param \Illuminate\Http\Request $request リクエスト
-     * @return void
-     */
-    public function create(Request $request)
-    {
-
-        // アップロードされたかチェック(アップロードされた場合は該当の項目にファイル名をセットする)
-        $this->fileUploadSetVal($request, 'upload_file');
-
-        // 登録前バリデーション。NGの場合はレスポンスコード422を返却
-        Validator::make($request->all(), $this->rulesForInput())->validate();
-
-        // アップロード先(アップ先は用途ごとに分ける)
-        $uploadDir = config('appconf.upload_dir_all_member_import') . date("YmdHis");
-
-        // アップロードファイルの保存
-        $path = $this->fileUploadSave($request, $uploadDir, 'upload_file');
-        $base_path = base_path();
-
-        // 実行者のアカウントIDを取得
-        $account_id = Auth::user()->account_id;
-
-        // 非同期実行する
-        if (strpos(PHP_OS, 'WIN') !== false) {
-            // Windows
-            $command = 'start /b /d ' . base_path() . ' php artisan command:allMemberImport ' . $path . ' ' . $account_id;
-            $fp = popen($command, 'r');
-            pclose($fp);
-        } else {
-            // Linux
-            $command = "cd {$base_path} && php artisan command:allMemberImport {$path} {$account_id} > /dev/null &";
-            exec($command);
-        }
-
-        return;
-    }
-
-    /**
-     * バリデーション(取込用)
-     *
-     * @param \Illuminate\Http\Request $request リクエスト
-     * @return mixed バリデーション結果
-     */
-    public function validationForInput(Request $request)
-    {
-
-        // アップロードされたかチェック(アップロードされた場合は該当の項目にファイル名をセットする)
-        $this->fileUploadSetVal($request, 'upload_file');
-
-        // リクエストデータチェック
-        $validator = Validator::make($request->all(), $this->rulesForInput());
-
-        // エラーがあれば返却
-        if ($validator->fails()) {
-            return $validator->errors();
-        }
-
-        // バッチ実行中の場合はエラー
-        try {
-            $exists = BatchMng::where('batch_type', '=', AppConst::BATCH_TYPE_1)
-                ->where('batch_state', '=', AppConst::CODE_MASTER_22_99)
-                ->exists();
-
-            if ($exists) {
-                throw new ReadDataValidateException(Lang::get('validation.already_running'));
-            }
-        } catch (ReadDataValidateException $e) {
-            // ファイルのバリデーションエラーとして返却
-            return ['upload_file' => [$e->getMessage()]];
-        }
-
-        // パスを取得(upload直後のtmpのパス)
-        $path = $this->fileUploadRealPath($request, 'upload_file');
-        try {
-            // Zipを解凍し、ファイルパス一覧を取得
-            $opPathList = $this->unzip($path);
-            // 今回は1件しか無いので、1件目を取得
-            if (count($opPathList) != 1) {
-                throw new ReadDataValidateException(Lang::get('validation.invalid_file')
-                    . "(csvファイル数不正)");
-            }
-            $csvPath = $opPathList[0];
-
-            // CSVの中身の読み込みとバリデーション
-            $this->readData($csvPath);
-
-            // Zip解凍ファイルのクリーンアップ
-            $this->unzipCleanUp($opPathList);
-        } catch (ReadDataValidateException $e) {
-            // ファイルのバリデーションエラーとして返却
-            return ['upload_file' => [$e->getMessage()]];
-        }
-
-        return [];
-    }
-
-    /**
-     * アップロードされたファイルを読み込む
-     * バリデーションも行う
-     *
-     * @param string $path ファイルパス
-     * @return void
-     */
-    private function readData($path)
-    {
-
-        // ヘッダ行
-        $csvHeaders = ["sid", "name", "cls_cd", "mailaddress1", "disp_flg", "updtime", "upduser", "enter_date"];
-
-        $headers = [];
-
-        // CSV読み込み
-        $file = $this->readCsv($path, "sjis");
-
-        // 1行ずつ取得
-        foreach ($file as $i => $line) {
-
-            // 1行目がヘッダ行
-            if ($i === 0) {
-                //-------------
-                // ヘッダ行
-                //-------------
-                $headers = $line;
-
-                // [バリデーション] ヘッダが想定通りかチェック
-                if ($headers !== $csvHeaders) {
-                    throw new ReadDataValidateException(Lang::get('validation.invalid_file')
-                        . "(" . config('appconf.upload_file_csv_name_A05') . "：ヘッダ行不正)");
-                }
-
-                continue;
-            }
-
-            // [バリデーション] データ行の列の数のチェック
-            if (count($line) !== count($csvHeaders)) {
-                throw new ReadDataValidateException(Lang::get('validation.invalid_file')
-                    . "(" . config('appconf.upload_file_csv_name_A05') . "：データ列数不正)");
-            }
-
-            // headerをもとに、値をセットしたオブジェクトを生成
-            $values = array_combine($headers, $line);
-
-            // [バリデーション] データ行の値のチェック
-            $rules = [];
-            $rules += ExtStudentKihon::fieldRules('sid', ['required']);
-            $rules += ExtStudentKihon::fieldRules('name', ['required']);
-            $rules += ExtStudentKihon::fieldRules('cls_cd', ['required']);
-            $rules += ExtStudentKihon::fieldRules('mailaddress1', ['required']);
-            $rules += ExtStudentKihon::fieldRules('enter_date', [], '_csv');
-            $rules += ExtStudentKihon::fieldRules('disp_flg', ['required']);
-            $rules += ExtStudentKihon::fieldRules('updtime', ['required'], '_csv');
-
-            $validator = Validator::make($values, $rules);
-            if ($validator->fails()) {
-                throw new ReadDataValidateException(Lang::get('validation.invalid_file')
-                . config('appconf.upload_file_csv_name_A05')
-                . "：データ項目不正( 生徒No=" . $values['sid'] . " )");
-            }
-
-            // 10行目までチェックする
-            if ($i === 10) {
-                break;
-            }
-        }
-
-        return;
-    }
-
-    /**
-     * バリデーションルールを取得(取込用)
-     *
-     * @return array ルール
-     */
-    private function rulesForInput()
-    {
-
-        $rules = array();
-
-        // 独自バリデーション: ファイル名のチェック
-        $validationFileName = function ($attribute, $value, $fail) {
-
-            // ファイル名の先頭をチェック
-            // 想定：年次学年情報_20201124114040.zip
-            $fileName = config('appconf.upload_file_name_all_member_import_enter');
-            if (!preg_match('/^' . $fileName . '[0-9]{14}.zip$/', $value)) {
-                return $fail(Lang::get('validation.invalid_file'));
-            }
-        };
-
-        //-----------------------------
-        // ファイルアップロード
-        //-----------------------------
-
-        // ファイルアップロードの必須チェック
-        $rules += ['upload_file' => ['required', $validationFileName]];
-
-        // ファイルのタイプのチェック(「file_項目名」の用にチェックする)
-        $rules += ['file_upload_file' => [
-            // ファイル
-            'file',
-            // 拡張子
-            'mimes:zip',
-            // Laravelが判定したmimetypes
-            'mimetypes:application/zip',
-        ]];
-
-        return $rules;
-    }
 }
