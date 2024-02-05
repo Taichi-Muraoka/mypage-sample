a338c405ebc6245abb07484aceda4e9f62ccea96
taichi.muraoka <taichi.okamu@gmail.com>
2024-02-05 10:49:01 +0900

    #29 修正 - 生徒記録管理（運用管理） Logのuse宣言追加

--- source/web-testea/app/Http/Controllers/Admin/RecordController.php
+++ source/web-testea/app/Http/Controllers/Admin/RecordController.php
@@ -7,9 +7,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\Log;
 use App\Models\Record;
 use App\Consts\AppConst;
-use Illuminate\Support\Facades\Lang;
 use App\Http\Controllers\Traits\FuncRecordTrait;
 
 /**
