39676f159ceaf69621454d9ebc4ba37a9d6255c4
taichi.muraoka <taichi.okamu@gmail.com>
2024-02-06 09:34:41 +0900

    DBのuse宣言追加

--- source/web-testea/app/Http/Controllers/Student/ConferenceController.php
+++ source/web-testea/app/Http/Controllers/Student/ConferenceController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 use App\Models\Conference;
 use App\Models\ConferenceDate;
 
