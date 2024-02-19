4b881d958012d1880c92e49ba12a442cd035b887
coworksnabe <atsuki.watanabe@co-works.co.jp>
2024-02-19 14:08:57 +0900

    #240 修正 - 欠席連絡（生徒向け） 授業日プルダウン選択時の授業情報表示の動作統一化 ・授業日時選択時にテーブル表示されるよう修正 ・「講師」→「講師名」

--- source/web-testea/resources/views/pages/student/absent.blade.php
+++ source/web-testea/resources/views/pages/student/absent.blade.php
@@ -17,7 +17,7 @@
 
     {{-- 詳細を表示 --}}
     <div v-cloak>
-        <x-bs.table :hover=false :vHeader=true class="mb-4">
+        <x-bs.table :hover=false :vHeader=true class="mb-4" vShow="form.schedule_id">
             <tr>
                 <th width="35%">校舎</th>
                 <td><span v-cloak>@{{selectGetItem.campus_name}}</span></td>
@@ -27,7 +27,7 @@
                 <td><span v-cloak>@{{selectGetItem.course_name}}</span></td>
             </tr>
             <tr>
-                <th>講師</th>
+                <th>講師名</th>
                 <td><span v-cloak>@{{selectGetItem.tutor_name}}<span v-if="selectGetItem.teacher_name">先生</span></span>
                 </td>
             </tr>
