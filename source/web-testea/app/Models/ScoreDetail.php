9a7f492a776c52bf1c053324ab2a6585973b23db
Mikiko Toba <mikiko.toba@co-works.co.jp>
2024-02-22 19:12:11 +0900

    #256 修正 - 生徒成績 小数点入力対応

--- source/web-testea/app/Models/ScoreDetail.php
+++ source/web-testea/app/Models/ScoreDetail.php
@@ -109,8 +109,8 @@ class ScoreDetail extends Model
             'g_subject_cd' => ['string', 'max:3', 'digits:3'],
             'score' => ['integer', 'max:9999'],
             'full_score' => ['integer', 'max:9999'],
-            'average' => ['integer', 'max:9999'],
-            'deviation_score' => ['integer', 'max:9999']
+            'average' => ['numeric', 'max:9999.9'],
+            'deviation_score' => ['numeric', 'max:9999.9']
         ];
         return $_fieldRules;
     }
