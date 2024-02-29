1dc77586ca568abe9eb78f67ff60e093f96c3292
Mikiko Toba <mikiko.toba@co-works.co.jp>
2024-02-29 11:42:48 +0900

    #172 実装 - 保持期限超過データ削除 スケジューラ設定

--- source/web-testea/app/Console/Kernel.php
+++ source/web-testea/app/Console/Kernel.php
@@ -8,6 +8,18 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        Commands\GradeUpdate::class,
+        Commands\TransferReset::class,
+        Commands\ExceedingDataDelete::class,
+        Commands\YearInitialDataInsert::class
+    ];
+
+    /**
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
@@ -15,7 +27,17 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // 生徒学年更新 毎年3月1日午前0時に実行される
+        $schedule->command('command:gradeUpdate')->yearlyOn(3, 1, '00:00');
+
+        // 振替残数リセット 毎年3月1日午前0時10分に実行される
+        $schedule->command('command:transferReset')->yearlyOn(3, 1, '00:10');
+
+        // 保持期限超過データ削除 毎年3月1日午前0時20分に実行される
+        $schedule->command('command:exceedingDataDelete')->yearlyOn(3, 1, '00:20');
+
+        // 年次初期レコード作成 毎年3月1日午前0時40分に実行される
+        $schedule->command('command:yearInitialDataInsert')->yearlyOn(3, 1, '00:40');
     }
 
     /**
