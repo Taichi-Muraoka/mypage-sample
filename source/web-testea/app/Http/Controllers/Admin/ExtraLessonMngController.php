23f1b591da67eac80a95fd740b690fd8b54c9649
coworksnabe <atsuki.watanabe@co-works.co.jp>
2024-04-08 15:51:38 +0900

    #366 修正 - 追加授業調整機能(運用管理)　追加授業スケジュール登録

--- source/web-testea/app/Http/Controllers/Admin/ExtraLessonMngController.php
+++ source/web-testea/app/Http/Controllers/Admin/ExtraLessonMngController.php
@@ -703,6 +703,26 @@ class ExtraLessonMngController extends Controller
      */
     private function rulesForInput(?Request $request)
     {
+        // 独自バリデーション: リストのチェック 生徒
+        $validationStudentList =  function ($attribute, $value, $fail) use ($request) {
+            // 生徒リストを取得
+            $list = $this->mdlGetStudentList();
+            if (!isset($list[$value])) {
+                // 不正な値エラー
+                return $fail(Lang::get('validation.invalid_input'));
+            }
+        };
+
+        // 独自バリデーション: リストのチェック 校舎
+        $validationRoomList =  function ($attribute, $value, $fail) {
+            // 校舎リストを取得
+            $list = $this->mdlGetRoomList(false);
+            if (!isset($list[$value])) {
+                // 不正な値エラー
+                return $fail(Lang::get('validation.invalid_input'));
+            }
+        };
+
         // 独自バリデーション: リストのチェック 時限
         $validationPeriodList =  function ($attribute, $value, $fail) use ($request) {
             // 時限リストを取得（校舎・日付から）
@@ -758,6 +778,8 @@ class ExtraLessonMngController extends Controller
         // 追加授業スケジュール登録画面のバリデーション
         $rules += Schedule::fieldRules('memo');
         if ($request && !isset($request['status'])) {
+            $rules += Schedule::fieldRules('student_id', ['required', $validationStudentList]);
+            $rules += Schedule::fieldRules('campus_cd', ['required', $validationRoomList]);
             $rules += Schedule::fieldRules('target_date', ['required']);
             $rules += Schedule::fieldRules('period_no', ['required', $validationPeriodList]);
             $rules += Schedule::fieldRules('start_time', ['required']);
